4d5770b461b7b4da9048fa110d7616c6f40e3d5c
coherentus <75427478+coherentus@users.noreply.github.com>
2022-02-06 16:35:14 +0300

    fdgd

--- backend/api/paginations.py
+++ backend/api/paginations.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class PageLimitNumberPagination(PageNumberPagination):
     page_size_query_param = 'limit'
-    # page_query_param = 'page'
+    page_query_param = 'page'
     #page_size = 4
