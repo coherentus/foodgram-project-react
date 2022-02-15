e89159492faa606ebed03e10c983864cb248695e
Ковылин Василий <coherentus@gmail.com>
2022-02-15 14:29:56 +0300

    some fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -122,7 +122,7 @@ class RecipeSerializer(serializers.ModelSerializer):
             return obj.basket_recipes.filter(user=user).exists()
         return False
 
-    def validate_tags(self, data):
+    """def validate_tags(self, data):
         if not data:
             raise serializers.ValidationError(
                 'Ошибка: Создание рецепта без тега невозможно'
@@ -138,7 +138,7 @@ class RecipeSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(
                     f'Ошибка: Тега с указанным id = {tag_id} не существует'
                 )
-        return data
+        return data"""
 
     def validate_ingredients(self, data):
         ingredients = self.initial_data.get('ingredients')
