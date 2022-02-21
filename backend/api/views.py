dfee9f9e1e47e7c76d65642bd078a6f4dcd594e8
Ковылин Василий <coherentus@gmail.com>
2022-02-21 16:04:55 +0300

    edit imports in views

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,10 +7,19 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import (
-    HTTP_200_OK,
-    HTTP_201_CREATED,
-)
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from djoser.views import UserViewSet
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from logic.models import Basket, FavourRecipe, Follow
