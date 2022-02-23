489477cfed36a504d5f4172af156c444155420bf
coherentus <75427478+coherentus@users.noreply.github.com>
2022-02-23 19:48:57 +0300

    serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -19,7 +19,7 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
-        # read_only_fields = ('id', 'name', 'color', 'slug')
+        read_only_fields = ('id', 'name', 'color', 'slug')
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -350,14 +350,11 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
     """
 
     def __init__(self, *args, **kwargs):
-        # Don't pass the 'fields' arg up to the superclass
         fields = kwargs.pop('fields', None)
 
-        # Instantiate the superclass normally
         super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
 
         if fields is not None:
-            # Drop any fields that are not specified in the `fields` argument.
             allowed = set(fields)
             existing = set(self.fields)
             for field_name in existing - allowed:
