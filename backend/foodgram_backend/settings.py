b8e99babf7579e75cb02518153c90ca3798b02f6
coherentus <coherentus@gmail.com>
2022-02-27 23:17:33 +0300

    edit settings

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -118,6 +118,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+
 DJOSER = {
     'LOGIN_FIELD': 'email',
     'HIDE_USERS': False,
