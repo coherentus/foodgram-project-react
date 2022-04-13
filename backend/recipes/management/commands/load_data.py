e0b35283517fb1c2cedc779672a01951224b8290
Ковылин Василий <coherentus@gmail.com>
2022-04-13 08:38:05 +0300

    update load_data

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -2,14 +2,29 @@ import csv
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Product
+from recipes.models import Product, Tag
 
 
 class Command(BaseCommand):
     help = 'Load ingredients data to DB'
 
     def handle(self, *args, **options):
-        with open('../data/ingredients.csv', encoding='utf-8') as f:
+        print('Обработка файла tags.csv')
+        with open('./data/tags.csv', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            count = 0
+            for row in reader:
+                name, color, slug = row
+                Tag.objects.get_or_create(
+                    name=name,
+                    color=color,
+                    slug=slug
+                )
+                count += 1
+                print(f'Обработано: {count} записей.')    
+
+        print('Обработка файла ingredients.csv')
+        with open('./data/ingredients.csv', encoding='utf-8') as f:
             reader = csv.reader(f)
             count = 0
             for row in reader:
@@ -18,3 +33,4 @@ class Command(BaseCommand):
                 count += 1
                 if not count % 100:
                     print(f'Обработано: {count} записей.')
+        print(f'Всего обработано: {count} записей.')
